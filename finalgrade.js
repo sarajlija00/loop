00d65d395581e548a2a758bcc62f186b0ba41c2b
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-02-17 15:27:05 +0100

    ispravka

--- finalgrade.js
+++ finalgrade.js
@@ -1,20 +1,18 @@
 function finalGrade (exam,projects) {
-  if (exam > 90 && projects < 10 ){
+  if (exam > 90 && projects > 10 ){
     return 100;    
-    }
+}
   if (exam > 75 && projects <= 5) {
     return 90;
-    }
+}
   if (exam > 50 && projects <= 2) {
     return 75;      
-    }
-    return 0;
-    } 
-
 }
+    return 0;
+} 
 
-
-
+console.log (finalGrade);
+    
 finalGrade (100, 12);
 finalGrade (99, 0);
 finalGrade (10, 15);
