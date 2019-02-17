e9e0debe82bd63ebff0992bdc3d2647158c6041a
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-02-17 16:47:53 +0100

    valjda je sad ok

--- finalgrade.js
+++ finalgrade.js
@@ -1,11 +1,11 @@
 function finalGrade (exam,projects) {
-  if (exam > 90 && projects > 10 ){
+  if (exam > 90 II projects > 10 ){
     return 100;    
 }
-  if (exam > 75 && projects <= 5) {
+  if (exam > 75 && projects >= 5) {
     return 90;
 }
-  if (exam > 50 && projects <= 2) {
+  if (exam > 50 && projects >= 2) {
     return 75;      
 }
     return 0;
