f5d235cf32657490af7ab736258b05c31cfc19fd
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-02-18 12:18:56 +0100

    fwhile

---
+++ faktorijelwhile.js
@@ -0,0 +1,10 @@
+let result = 1;
+let number = 1;
+function factorial (n) {
+  while (number <= n) {
+    result = number * result, number++;   
+  } console.log (result);
+    
+}
+factorial (5);
+
